95c2fc161becdbfb9b7e82b0957c5a47f4fafc0d
Jeroen Bakker <j.bakker@atmind.nl>
2023-12-11 22:01:57 +0100

    Use VecDeque

--- audio-engine-common/src/buffer/ring_buffer.rs
+++ audio-engine-common/src/buffer/ring_buffer.rs
@@ -1,16 +1,14 @@
-use std::ops::AddAssign;
+use std::{collections::VecDeque, ops::AddAssign};
+
+// Use VecDeque
+
+//pub type RingBuffer<T> = VecDeque<T>;
 
 pub struct RingBuffer<T>
 where
     T: Sized,
 {
-    pub data: Vec<T>,
-
-    /// Start of the ring buffer in the data attribute.
-    pub start: usize,
-
-    /// End of the ring buffer in the data attribute.
-    pub end: usize,
+    pub data: VecDeque<T>,
 }
 
 impl<T> Default for RingBuffer<T>
@@ -19,9 +17,7 @@ where
 {
     fn default() -> Self {
         RingBuffer::<T> {
-            data: Vec::<T>::default(),
-            start: 0,
-            end: 0,
+            data: VecDeque::<T>::default(),
         }
     }
 }
@@ -31,6 +27,7 @@ pub enum PushOperation {
     Replace,
     Add,
 }
+
 impl PushOperation {
     fn perform<T: Copy + AddAssign>(&self, value: T, r_value: &mut T) {
         match self {
@@ -45,19 +42,15 @@ where
     T: Sized,
 {
     pub fn len(&self) -> usize {
-        if self.end >= self.start {
-            self.end - self.start
-        } else {
-            self.capacity() - self.start + self.end
-        }
+        self.data.len()
     }
 
     pub fn is_empty(&self) -> bool {
-        self.start == self.end
+        self.data.is_empty()
     }
 
     pub fn capacity(&self) -> usize {
-        self.data.len()
+        self.data.capacity()
     }
 }
 
@@ -66,66 +59,19 @@ where
     T: Sized + Default + Copy + AddAssign,
 {
     pub fn with_capacity(capacity: usize) -> RingBuffer<T> {
-        let mut result = RingBuffer::<T>::default();
-        result.ensure_capacity(capacity);
-        result
-    }
-
-    fn reserve(&mut self, capacity: usize) {
-        let mut new_data = Vec::<T>::with_capacity(capacity);
-        let len = self.len();
-
-        for i in self.start..self.data.len() {
-            new_data.push(self.data[i]);
+        RingBuffer::<T> {
+            data: VecDeque::<T>::with_capacity(capacity),
         }
-        if self.end < self.start {
-            for i in 0..self.end {
-                new_data.push(self.data[i]);
-            }
-        }
-        for _i in 0..capacity - len {
-            new_data.push(T::default());
-        }
-
-        self.data = new_data;
-        self.start = 0;
-        self.end = len;
-    }
-
-    pub fn ensure_capacity(&mut self, offset: usize) {
-        if self.capacity() < offset {
-            self.reserve(offset)
-        }
-    }
-
-    fn calc_offset(&self, offset: usize) -> usize {
-        (self.start + offset) % self.capacity()
     }
 
     /// Push a value into the ring buffer so that the value will be popped
     pub fn push(&mut self, offset: usize, value: T, operation: PushOperation) {
-        assert!(self.capacity() > offset);
-        let offset = self.calc_offset(offset);
-
-        operation.perform(value, &mut self.data[offset]);
-        self.end = (offset + 1) % self.capacity();
+        unimplemented!("add elements that don't exist");
+        operation.perform(value, self.data.get_mut(offset).unwrap());
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        if self.start == self.end {
-            None
-        } else {
-            let result = self.data[self.start];
-            self.data[self.start] = T::default();
-            self.skip();
-            Some(result)
-        }
-    }
-
-    pub fn skip(&mut self) {
-        if self.start != self.end {
-            self.start = (self.start + 1) % self.capacity();
-        }
+        self.data.pop_front()
     }
 
     pub fn pop_or_default(&mut self) -> T {
@@ -143,99 +89,33 @@ mod test {
     #[test]
     fn pop_0() {
         let mut buffer = RingBuffer::<i32>::with_capacity(5);
-        assert_eq!(buffer.start, 0);
-        assert_eq!(buffer.end, 0);
-        assert_eq!(buffer.len(), 0);
 
         buffer.push(0, 1, PushOperation::Add);
-        assert_eq!(buffer.start, 0);
-        assert_eq!(buffer.end, 1);
-        assert_eq!(buffer.len(), 1);
-
         assert_eq!(buffer.pop(), Some(1));
-        assert_eq!(buffer.start, 1);
-        assert_eq!(buffer.end, 1);
-        assert_eq!(buffer.len(), 0);
-
         assert_eq!(buffer.pop(), None);
-        assert_eq!(buffer.start, 1);
-        assert_eq!(buffer.end, 1);
-        assert_eq!(buffer.len(), 0);
     }
 
     #[test]
     fn pop_0_multiple() {
         let mut buffer = RingBuffer::<i32>::with_capacity(5);
-        assert_eq!(buffer.start, 0);
-        assert_eq!(buffer.end, 0);
-        assert_eq!(buffer.len(), 0);
-
-        buffer.ensure_capacity(5);
         buffer.push(0, 1, PushOperation::Add);
-        assert_eq!(buffer.start, 0);
-        assert_eq!(buffer.end, 1);
-        assert_eq!(buffer.len(), 1);
-
         let result = buffer.pop();
         assert_eq!(result, Some(1));
-        assert_eq!(buffer.start, 1);
-        assert_eq!(buffer.end, 1);
-        assert_eq!(buffer.len(), 0);
-
         buffer.push(0, 1, PushOperation::Add);
-        assert_eq!(buffer.start, 1);
-        assert_eq!(buffer.end, 2);
-        assert_eq!(buffer.len(), 1);
-
         let result = buffer.pop();
         assert_eq!(result, Some(1));
-        assert_eq!(buffer.start, 2);
-        assert_eq!(buffer.end, 2);
-        assert_eq!(buffer.len(), 0);
-
         buffer.push(0, 1, PushOperation::Add);
-        assert_eq!(buffer.start, 2);
-        assert_eq!(buffer.end, 3);
-        assert_eq!(buffer.len(), 1);
-
         let result = buffer.pop();
         assert_eq!(result, Some(1));
-        assert_eq!(buffer.start, 3);
-        assert_eq!(buffer.end, 3);
-        assert_eq!(buffer.len(), 0);
-
         buffer.push(0, 1, PushOperation::Add);
-        assert_eq!(buffer.start, 3);
-        assert_eq!(buffer.end, 4);
-        assert_eq!(buffer.len(), 1);
-
         let result = buffer.pop();
         assert_eq!(result, Some(1));
-        assert_eq!(buffer.start, 4);
-        assert_eq!(buffer.end, 4);
-        assert_eq!(buffer.len(), 0);
-
         buffer.push(0, 1, PushOperation::Add);
-        assert_eq!(buffer.start, 4);
-        assert_eq!(buffer.end, 0);
-        assert_eq!(buffer.len(), 1);
-
         let result = buffer.pop();
         assert_eq!(result, Some(1));
-        assert_eq!(buffer.start, 0);
-        assert_eq!(buffer.end, 0);
-        assert_eq!(buffer.len(), 0);
-
         buffer.push(0, 1, PushOperation::Add);
-        assert_eq!(buffer.start, 0);
-        assert_eq!(buffer.end, 1);
-        assert_eq!(buffer.len(), 1);
-
         let result = buffer.pop();
         assert_eq!(result, Some(1));
-        assert_eq!(buffer.start, 1);
-        assert_eq!(buffer.end, 1);
-        assert_eq!(buffer.len(), 0);
     }
 
     #[test]
