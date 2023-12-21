2e69a63e84f20540e32860703b80b2da45561f72
Jeroen Bakker <jeroen@blender.org>
2023-12-21 08:47:45 +0100

    Added comment

--- audio-engine-export-video/src/lib.rs
+++ audio-engine-export-video/src/lib.rs
@@ -1,1 +1,6 @@
+//! Export audio to a video file.
+//! 
+//! NOTE: Currently it only renders a video stream. Audio stream rendering should be added
+//! by using ffmpeg directly See https://github.com/larksuite/rsmpeg/blob/master/tests/avio_writing.rs
+//! for an example how we could use ffmpeg for this.
 pub mod export;
