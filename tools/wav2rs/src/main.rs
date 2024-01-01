b51694f567afaae24f74b25b96dd17c04943e76f
Jeroen Bakker <j.bakker@atmind.nl>
2024-01-01 11:08:47 +0100

    Add normalization when converting wav files.

--- tools/wav2rs/src/main.rs
+++ tools/wav2rs/src/main.rs
@@ -17,6 +17,7 @@ fn main() {
     let floats = data.to_f32s();
     let floats = join_channels(header, &floats);
     let floats = trim_start(&floats);
+    let floats = normalize(&floats);
     let floats_len = floats.len();
     println!("pub static SAMPLES: [f32;{floats_len}] = [");
     for float in floats {
@@ -26,18 +27,31 @@ fn main() {
 }
 
 fn join_channels(header: Header, data: &[f32]) -> Vec<f32> {
-    let mut samples = Vec::new();
-    for channel_samples in data.chunks(header.channel_count as usize) {
-        let sample = channel_samples.iter().sum::<f32>();
-        samples.push(sample);
-    }
-
-    samples
+    data.chunks(header.channel_count as usize)
+        .map(|chunk| chunk.iter().sum::<f32>())
+        .collect::<Vec<f32>>()
 }
 
 fn trim_start(data: &[f32]) -> Vec<f32> {
     data.iter()
         .skip_while(|sample| **sample == 0.0)
         .cloned()
+        .collect::<Vec<f32>>()
+}
+
+fn normalize(data: &[f32]) -> Vec<f32> {
+    let mut max_amplitude: f32 = 0.0;
+    data.iter()
+        .map(|sample| sample.abs())
+        .for_each(|sample| max_amplitude = max_amplitude.max(sample));
+
+    let multiplier = if max_amplitude == 0.0 {
+        1.0
+    } else {
+        1.0 / max_amplitude
+    };
+
+    data.iter()
+        .map(|sample| sample * multiplier)
         .collect::<Vec<f32>>()
 }
