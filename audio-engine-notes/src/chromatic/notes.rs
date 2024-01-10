2a01b161bc3dc0c91ac214337e02ab60606bdc2e
Jeroen Bakker <j.bakker@atmind.nl>
2024-01-10 12:53:22 +0100

    Convert test to doctest

--- audio-engine-notes/src/chromatic/notes.rs
+++ audio-engine-notes/src/chromatic/notes.rs
@@ -9,29 +9,33 @@ impl Default for ChromaticNote {
 }
 
 impl ChromaticNote {
-    pub fn multiplier(&self) -> f32 {
-        match self.octave {
-            0 => 1.0 / 16.0,
-            1 => 1.0 / 8.0,
-            2 => 1.0 / 4.0,
-            3 => 1.0 / 2.0,
-            4 => 1.0,
-            5 => 2.0,
-            6 => 4.0,
-            7 => 8.0,
-            8 => 16.0,
-            _ => 1.0,
-        }
+    /// Calculate the multiplier to move a tone (tuned at octave 4) to the octave in self.octave.
+    ///
+    /// ```
+    /// use audio_engine_notes::{ChromaticNote, ChromaticTone};
+    /// assert_eq!(1.0 / 16.0, ChromaticNote::new(ChromaticTone::C, 0).multiplier_octave4());
+    /// assert_eq!(1.0 / 8.0, ChromaticNote::new(ChromaticTone::C, 1).multiplier_octave4());
+    /// assert_eq!(1.0 / 4.0, ChromaticNote::new(ChromaticTone::C, 2).multiplier_octave4());
+    /// assert_eq!(1.0 / 2.0, ChromaticNote::new(ChromaticTone::C, 3).multiplier_octave4());
+    /// assert_eq!(1.0, ChromaticNote::new(ChromaticTone::C, 4).multiplier_octave4());
+    /// assert_eq!(2.0, ChromaticNote::new(ChromaticTone::C, 5).multiplier_octave4());
+    /// assert_eq!(4.0, ChromaticNote::new(ChromaticTone::C, 6).multiplier_octave4());
+    /// assert_eq!(8.0, ChromaticNote::new(ChromaticTone::C, 7).multiplier_octave4());
+    /// assert_eq!(16.0, ChromaticNote::new(ChromaticTone::C, 8).multiplier_octave4());
+    /// ```
+    pub fn multiplier_octave4(&self) -> f32 {
+        2.0_f32.powi(self.octave as i32 - 4)
     }
-    pub fn pitch(&self) -> f32 {
-        self.tone.pitch_octave4() * self.multiplier()
-    }
-}
 
-/// Test to check that A4 is at 440 hz.
-#[test]
-fn chromatic_note_pitch_a4() {
-    let a4 = ChromaticNote::new(ChromaticTone::A, 4);
-    let pitch = a4.pitch();
-    assert_eq!(pitch, 440.0);
+    /// Get the pitch of self.
+    ///
+    /// Result is in Hz.
+    ///
+    /// ```
+    /// use audio_engine_notes::{ChromaticNote, ChromaticTone};
+    /// assert_eq!(440.0, ChromaticNote::new(ChromaticTone::A, 4).pitch());
+    /// ```
+    pub fn pitch(&self) -> f32 {
+        self.tone.pitch_octave4() * self.multiplier_octave4()
+    }
 }
