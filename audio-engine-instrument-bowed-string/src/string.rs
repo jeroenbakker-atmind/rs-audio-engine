c3da25cf42b448998dc74698f984691cbb2ed232
Jeroen Bakker <jeroen@blender.org>
2024-01-17 10:10:13 +0100

    Checkin before refactor

--- audio-engine-instrument-bowed-string/src/string.rs
+++ audio-engine-instrument-bowed-string/src/string.rs
@@ -1,8 +1,13 @@
 #[derive(Debug, Default, Copy, Clone)]
 pub struct String {
+    /// Radius of the string in meters.
     pub radius: f32,
+    /// Density of the string in kg/m.
     pub density: f32,
+    /// Tension of the string in newtons.
     pub tension: f32,
+    /// Young modulus of the string in Pa.
     pub young_mod: f32,
+    /// Length of the string in meters.
     pub length: f32,
 }
--- audio-engine-instrument-bowed-string/src/test.rs
+++ audio-engine-instrument-bowed-string/src/test.rs
@@ -3,17 +3,17 @@ use crate::{string::String, string_processor::StringProcessor};
 #[test]
 fn string_processor() {
     let string = String {
-        radius: 3.75e-04,
-        density: 3.7575e3,
-        tension: 153.0,
-        young_mod: 25e9,
+        radius: 7.2e-4,
+        density: 1.3017e4,
+        tension: 172.74,
+        young_mod: 22.4e9,
         length: 0.69,
     };
     let mut string_processor = StringProcessor::new(44100.0, &string);
     string_processor.vb = 0.2;
     string_processor.fb = 10.0;
-    string_processor.set_input_position(0.5);
-    string_processor.set_read_position(0.1);
+    string_processor.set_input_position(0.733);
+    string_processor.set_read_position(0.5);
     string_processor.is_being_played = true;
     string_processor.gain = 10.0;
     for _ in 0..4096 {
