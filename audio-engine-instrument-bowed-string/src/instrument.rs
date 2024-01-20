f5b56d3c906b243dd0fca34b6db63b0f194ae157
Jeroen Bakker <jeroen@blender.org>
2024-01-20 17:06:35 +0100

    some changes

--- audio-engine-instrument-bowed-string/src/instrument.rs
+++ audio-engine-instrument-bowed-string/src/instrument.rs
@@ -7,6 +7,7 @@ use crate::{
     bow::Bow,
     instrument_state::BowedStringInstrumentState,
     processor::StringProcessor,
+    sherman_morrison_processor::ShermanMorrison,
     string::{calc_hand_position_multiplier, String},
 };
 
@@ -35,9 +36,9 @@ impl Sound for BowedStringInstrument {
     fn sample(&self, parameters: &Self::Parameters, state: &mut BowedStringInstrumentState) -> f32 {
         if state.string_processors.is_empty() {
             for string in &self.strings {
-                state
-                    .string_processors
-                    .push(StringProcessor::new(parameters.sample_rate, string));
+                let mut processor = ShermanMorrison::new(parameters.sample_rate, string);
+                processor.gain = 1000000.0;
+                state.string_processors.push(processor);
             }
         }
 
@@ -55,11 +56,11 @@ impl Sound for BowedStringInstrument {
             .string_pitches
             .iter()
             .enumerate()
-            .filter(|(_, base_pitch)| base_pitch.frequency >= note_pitch.frequency)
+            .filter(|(_, base_pitch)| base_pitch.frequency < note_pitch.frequency)
             .max_by(|(_, a), (_, b)| {
-                if a.frequency > b.frequency {
+                if a.frequency < b.frequency {
                     Ordering::Greater
-                } else if a.frequency < b.frequency {
+                } else if a.frequency > b.frequency {
                     Ordering::Less
                 } else {
                     Ordering::Equal
@@ -67,13 +68,20 @@ impl Sound for BowedStringInstrument {
             })
         {
             let hand_position = calc_hand_position_multiplier(*base_pitch, note_pitch);
-            let processor = &mut state.string_processors[string_index];
-            
-            if parameters.note_off.is_none() {
-                // Apply pressure and hand position to the string.
-                processor.bow.velocity = 0.1;
-                processor.bow.pressure = 10.0 * parameters.gain;
-                processor.set_hand_position_multiplier(hand_position);
+            if state.last_string_index != string_index || state.last_hand_position != hand_position
+            {
+                state.last_string_index = string_index;
+                state.last_hand_position = hand_position;
+                println!(
+                    "position_altered(string_index={string_index},hand_position={hand_position})"
+                );
+                let processor = &mut state.string_processors[string_index];
+                if parameters.note_off.is_none() {
+                    // Apply pressure and hand position to the string.
+                    processor.bow.velocity = 0.1;
+                    processor.bow.pressure = 10.0 * parameters.gain;
+                    processor.set_hand_position_multiplier(hand_position);
+                }
             }
         }
 
@@ -82,10 +90,8 @@ impl Sound for BowedStringInstrument {
             .string_processors
             .iter_mut()
             .for_each(|processor| processor.compute_state());
-        state
-            .string_processors
-            .iter_mut()
-            .map(|processor| processor.read_output())
-            .sum::<f32>()
+        let result = state.string_processors[state.last_string_index].read_output();
+        // println!("{result}");
+        result
     }
 }
--- audio-engine-instrument-bowed-string/src/instrument_state.rs
+++ audio-engine-instrument-bowed-string/src/instrument_state.rs
@@ -6,12 +6,14 @@ use crate::sherman_morrison_processor::ShermanMorrison;
 pub struct BowedStringInstrumentState {
     pub string_processors: Vec<ShermanMorrison>,
     pub last_hand_position: f32,
+    pub last_string_index: usize,
 }
 impl Default for BowedStringInstrumentState {
     fn default() -> Self {
         Self {
             string_processors: Vec::default(),
             last_hand_position: 1.0,
+            last_string_index: 100,
         }
     }
 }
