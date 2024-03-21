96987baad106d45d0600d299d716268966e1a776
Jeroen Bakker <j.bakker@atmind.nl>
2024-03-21 20:45:28 +0100

    Added DW1 test case. failing to add transfer functions

--- audio-engine-discrete-time/src/transfer_function.rs
+++ audio-engine-discrete-time/src/transfer_function.rs
@@ -70,11 +70,29 @@ impl Mul<f64> for TransferFunction {
         }
     }
 }
+impl Mul<f64> for &TransferFunction {
+    type Output = TransferFunction;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        TransferFunction {
+            timestep: self.timestep,
+            numerator: &self.numerator * rhs,
+            denominator: self.denominator.clone(),
+        }
+    }
+}
 
 impl Mul<TransferFunction> for f64 {
     type Output = TransferFunction;
 
     fn mul(self, rhs: TransferFunction) -> Self::Output {
+        rhs * self
+    }
+}
+impl Mul<&TransferFunction> for f64 {
+    type Output = TransferFunction;
+
+    fn mul(self, rhs: &TransferFunction) -> Self::Output {
         rhs * self
     }
 }
@@ -93,9 +111,51 @@ impl Mul<&TransferFunction> for &TransferFunction {
         }
     }
 }
+impl Mul<&TransferFunction> for TransferFunction {
+    type Output = TransferFunction;
+
+    fn mul(self, rhs: &TransferFunction) -> Self::Output {
+        assert_eq!(self.timestep, rhs.timestep);
+        let new_numerator = &self.numerator * &rhs.numerator;
+        let new_denominator = &self.denominator * &rhs.denominator;
+        TransferFunction {
+            timestep: self.timestep,
+            numerator: new_numerator,
+            denominator: new_denominator,
+        }
+    }
+}
+impl Mul<TransferFunction> for TransferFunction {
+    type Output = TransferFunction;
+
+    fn mul(self, rhs: TransferFunction) -> Self::Output {
+        assert_eq!(self.timestep, rhs.timestep);
+        let new_numerator = &self.numerator * &rhs.numerator;
+        let new_denominator = &self.denominator * &rhs.denominator;
+        TransferFunction {
+            timestep: self.timestep,
+            numerator: new_numerator,
+            denominator: new_denominator,
+        }
+    }
+}
 // #endregion
 
-// #region multiply
+// #region addition
+impl Add<TransferFunction> for TransferFunction {
+    type Output = TransferFunction;
+
+    fn add(self, rhs: TransferFunction) -> Self::Output {
+        let new_denominator = &self.denominator * &rhs.denominator;
+        let new_numerator = &self.numerator + &rhs.numerator;
+        TransferFunction {
+            timestep: self.timestep,
+            numerator: new_numerator,
+            denominator: new_denominator,
+        }
+    }
+}
+
 impl Add<f64> for &TransferFunction {
     type Output = TransferFunction;
 
@@ -117,6 +177,8 @@ impl Add<TransferFunction> for f64 {
     }
 }
 
+// #endregion
+// #region division
 impl Div<&TransferFunction> for f64 {
     type Output = TransferFunction;
 
@@ -131,17 +193,56 @@ impl Div<&TransferFunction> for f64 {
     }
 }
 
+impl Div<&TransferFunction> for &TransferFunction {
+    type Output = TransferFunction;
+
+    fn div(self, rhs: &TransferFunction) -> Self::Output {
+        let new_numerator = &rhs.denominator * &self.numerator;
+        let new_denominator = &self.denominator * &rhs.numerator;
+        TransferFunction {
+            timestep: rhs.timestep,
+            numerator: new_numerator,
+            denominator: new_denominator,
+        }
+    }
+}
+
+impl Div<&TransferFunction> for TransferFunction {
+    type Output = TransferFunction;
+
+    fn div(self, rhs: &TransferFunction) -> Self::Output {
+        assert!(self.timestep == rhs.timestep);
+        let new_numerator = &rhs.denominator * &self.numerator;
+        let new_denominator = &self.denominator * &rhs.numerator;
+        TransferFunction {
+            timestep: self.timestep,
+            numerator: new_numerator,
+            denominator: new_denominator,
+        }
+    }
+}
+impl Div<TransferFunction> for &TransferFunction {
+    type Output = TransferFunction;
+
+    fn div(self, rhs: TransferFunction) -> Self::Output {
+        assert!(self.timestep == rhs.timestep);
+        let new_numerator = &rhs.denominator * &self.numerator;
+        let new_denominator = &self.denominator * &rhs.numerator;
+        TransferFunction {
+            timestep: self.timestep,
+            numerator: new_numerator,
+            denominator: new_denominator,
+        }
+    }
+}
+
 impl Div<TransferFunction> for TransferFunction {
     type Output = TransferFunction;
 
     fn div(self, rhs: TransferFunction) -> Self::Output {
-        // TODO: ensure same denominators.
-        // octave does something else. it multiplies the numerator with the denumerators (shift) making the denumerators equal to 1.
-        // and then do the division. The multiplication ensures same denominators, but is not always needed IMO. But better to keep consistent.
-        assert!(self.denominator == rhs.denominator);
         assert!(self.timestep == rhs.timestep);
-        let new_numerator = &self.numerator * &self.denominator;
-        let new_denominator = &rhs.numerator * &rhs.denominator;
+        let new_numerator = &rhs.denominator * &self.numerator;
+        let new_denominator = &self.denominator * &rhs.numerator;
         TransferFunction {
             timestep: self.timestep,
             numerator: new_numerator,
